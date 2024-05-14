b232b1ca4b3da378c3732d93fbaf21652da9fae8
NomanGhaziani <153339067+NomanGhaziani@users.noreply.github.com>
2024-05-14 17:10:47 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,13 @@
+let student = [
+    {
+        name : 'noman',
+         rollnumber:'004112',
+        timing : '7pm to 10pm'
+    },
+    {
+        name : 'usman',
+        rollnumber : '002526',
+        timing : '7pm to 10pm'
+    }
+]
+console.log(student);
